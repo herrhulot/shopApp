b03f3cc1236a759fb9443a2ddad5ea7fea03b693
herrhulot <petterwallgren83@gmail.com>
2020-05-12 13:47:58 +0200

    Finished section 11

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/auth.dart';
 import 'package:provider/provider.dart';
 
+import './screens/splash_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -24,47 +25,53 @@ class MyApp extends StatelessWidget {
     // "instanciate" means we create a new object from the class Products().
     // If we use "value" here it can produce bugs and decrease performance.
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (ctx) => Auth(),
-          ),
-          ChangeNotifierProxyProvider<Auth, Products>(
-            update: (ctx, auth, previousProducts) => Products(
-                auth.token,
-                auth.userId,
-                previousProducts == null ? [] : previousProducts.items),
-            create: (_) => Products('', '', []),
-          ),
-          ChangeNotifierProvider(
-            create: (ctx) => Cart(),
-          ),
-          ChangeNotifierProxyProvider<Auth, Orders>(
-            update: (ctx, auth, previousOrders) => Orders(
-                auth.token,
-                auth.userId,
-                previousOrders == null ? [] : previousOrders.orders),
-            create: (_) => Orders('', '', []),
-          )
-        ],
-        child: Consumer<Auth>(
-            builder: (ctx, auth, _) => MaterialApp(
-                    debugShowCheckedModeBanner: false,
-                    title: 'MyShop',
-                    theme: ThemeData(
-                      primarySwatch: Colors.blueGrey,
-                      accentColor: Colors.deepOrange,
-                      fontFamily: 'Lato',
-                    ),
-                    home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
-                    routes: {
-                      ProductDetailScreen.routeName: (lemur) =>
-                          ProductDetailScreen(),
-                      CartScreen.routeName: (utter) => CartScreen(),
-                      OrdersScreen.routeName: (vessla) => OrdersScreen(),
-                      UserProductsScreen.routeName: (groundhog) =>
-                          UserProductsScreen(),
-                      EditProductScreen.routeName: (hund) =>
-                          EditProductScreen(),
-                    })));
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Products>(
+          update: (ctx, auth, previousProducts) => Products(
+              auth.token,
+              auth.userId,
+              previousProducts == null ? [] : previousProducts.items),
+          create: (_) => Products('', '', []),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Orders>(
+          update: (ctx, auth, previousOrders) => Orders(auth.token, auth.userId,
+              previousOrders == null ? [] : previousOrders.orders),
+          create: (_) => Orders('', '', []),
+        )
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'MyShop',
+            theme: ThemeData(
+              primarySwatch: Colors.blueGrey,
+              accentColor: Colors.deepOrange,
+              fontFamily: 'Lato',
+            ),
+            home: auth.isAuth
+                ? ProductsOverviewScreen()
+                : FutureBuilder(
+                    future: auth.tryAutoLogin(),
+                    builder: (ctx, autoResultSnapShot) =>
+                        autoResultSnapShot.connectionState ==
+                                ConnectionState.waiting
+                            ? SplashScreen()
+                            : AuthScreen(),
+                  ),
+            routes: {
+              ProductDetailScreen.routeName: (lemur) => ProductDetailScreen(),
+              CartScreen.routeName: (utter) => CartScreen(),
+              OrdersScreen.routeName: (vessla) => OrdersScreen(),
+              UserProductsScreen.routeName: (groundhog) => UserProductsScreen(),
+              EditProductScreen.routeName: (hund) => EditProductScreen(),
+            }),
+      ),
+    );
   }
 }
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/http_exception.dart';
 
@@ -8,6 +11,7 @@ class Auth extends ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -56,7 +60,18 @@ class Auth extends ChangeNotifier {
           seconds: int.parse(responseData['expiresIn']),
         ),
       );
+      _autoLogout();
       notifyListeners();
+      final prefs = await SharedPreferences.getInstance();
+      // JSON data is always stored as a "String" with "" arounds it.
+      final userData = json.encode(
+        {
+          'token': _token,
+          'userId': _userId,
+          'expiryDate': _expiryDate.toIso8601String(),
+        },
+      );
+      prefs.setString('userData', userData);
     } catch (error) {
       throw error;
     }
@@ -70,10 +85,45 @@ class Auth extends ChangeNotifier {
     return _authenticate(email, password, "signInWithPassword");
   }
 
-  void logout() {
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+    final extractedUserData =
+        json.decode(prefs.getString('userData')) as Map<String, Object>;
+    final expiryDate = DateTime.parse(extractedUserData['expiryDate']);
+
+    if (expiryDate.isBefore(DateTime.now())) {
+      return false;
+    }
+    _token = extractedUserData['token'];
+    _userId = extractedUserData['userId'];
+    _expiryDate = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
+  }
+
+  Future<void> logout() async {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    // prefs.remove('userData'); Below clears all data in Shared preferences.
+    prefs.clear();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
